1c4189fc79e83f971ea48a5cac04781234cbef4b
sergeyerofeev <sergeyexport@yandex.ru>
2024-02-02 14:45:08 +0300

    Creating a project

---
+++ pzem-004T_nokia5110/Core/Inc/nokia5110_font.h
@@ -0,0 +1,19 @@
+/*
+ * nokia5110_font.h
+ *
+ *  Created on: Jan 28, 2024
+ *      Author: Sergey
+ */
+
+#ifndef INC_NOKIA5110_FONT_H_
+#define INC_NOKIA5110_FONT_H_
+
+#define FONT_HEADER_SIZE  4
+
+extern const uint8_t TinyFont[];
+extern const uint8_t SmallFont[];
+extern const uint8_t MediumNumbers[];
+extern const uint8_t BigNumbers[];
+extern const uint8_t Bitmap[];
+
+#endif /* INC_NOKIA5110_FONT_H_ */
---
+++ pzem-004T_nokia5110/Core/Src/main.c
@@ -0,0 +1,380 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+#include "nokia5110.h"
+#include "nokia5110_font.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+SPI_HandleTypeDef hspi1;
+DMA_HandleTypeDef hdma_spi1_tx;
+
+UART_HandleTypeDef huart1;
+
+/* USER CODE BEGIN PV */
+uint8_t txData[] = { 0x01, 0x04, 0x00, 0x00, 0x00, 0x0A, 0x70, 0x0D };
+uint8_t rxData[25];
+
+uint8_t isReadyData = 0;
+uint8_t isErrorData = 0;
+
+float v; // Напряжение
+double a; // Ток
+double w; // Мощность
+int e; // Энергия
+float pf; // Коэффициент мощности
+
+char str[20];
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
+static void MX_USART1_UART_Init(void);
+static void MX_SPI1_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_USART1_UART_Init();
+  MX_SPI1_Init();
+  /* USER CODE BEGIN 2 */
+  N5110_Init();
+
+  HAL_UART_Transmit(&huart1, txData, 8, 100);
+  __HAL_UART_ENABLE_IT(&huart1, UART_IT_IDLE);
+  HAL_UART_Receive_IT(&huart1, rxData, 25);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1) {
+    if (isReadyData) {
+      isReadyData = 0;
+
+      // Напряжение, байты 3 и 4, один бит равен 0,1 В
+      v = (rxData[3] << 8 | rxData[4]) / 10.0;
+      // Ток, 5 и 6 младшие байты 7 и 8 старшие байты, один бит равен 0,001 А
+      a = ((uint64_t) rxData[7] << 32 | rxData[8] << 16 | rxData[5] << 8 | rxData[6]) / 1000.0;
+      // Мощность, 9 и 10 младшие байты, 11 и 12 старшие байты, один бит равен 0,1 Вт
+      w = ((uint64_t) rxData[11] << 32 | rxData[12] << 16 | rxData[9] << 8 | rxData[10]) / 10.0;
+      // Энергия, 13 и 14 младшие байты, 15 и 16 старшие байты, один бит равен 1 Вт*ч
+      e = (uint64_t) rxData[15] << 32 | rxData[16] << 16 | rxData[13] << 8 | rxData[14];
+      // Коэффициент мощности, байты 19 и 20, один бит равен 0,01
+      pf = (rxData[19] << 8 | rxData[20]) / 100.0;
+
+      N5110_Clear();
+      N5110_SetFont(SmallFont);
+
+      snprintf(str, 20, "U = %5.1f V", v);
+      N5110_PrintStr(N5110_MAX_WIDTH / 2 - (6 * 6), 0, str, true);
+
+      snprintf(str, 20, "I = %5.3f A", a);
+      N5110_PrintStr(N5110_MAX_WIDTH / 2 - (6 * 6), 10, str, true);
+
+      snprintf(str, 20, "P = %5.1f W", w);
+      N5110_PrintStr(N5110_MAX_WIDTH / 2 - (6 * 6), 20, str, true);
+
+      snprintf(str, 20, "E = %5d W*h", e);
+      N5110_PrintStr(N5110_MAX_WIDTH / 2 - (6 * 6), 30, str, true);
+
+      snprintf(str, 20, "PF = %4.2f", pf);
+      N5110_PrintStr(N5110_MAX_WIDTH / 2 - (4.5 * 6), 40, str, true);
+      N5110_Update();
+
+      HAL_UART_Transmit(&huart1, txData, 8, 100);
+      HAL_Delay(1000);
+      __HAL_UART_ENABLE_IT(&huart1, UART_IT_IDLE);
+      HAL_UART_Receive_IT(&huart1, rxData, 25);
+    }
+
+    if (isErrorData) {
+      isErrorData = 0;
+
+      HAL_UART_Transmit(&huart1, txData, 8, 100);
+      HAL_Delay(1000);
+      HAL_UART_Receive_IT(&huart1, rxData, 25);
+    }
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+  RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+   * in the RCC_OscInitTypeDef structure.
+   */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK) {
+    Error_Handler();
+  }
+
+  /** Enables the Clock Security System
+   */
+  HAL_RCC_EnableCSS();
+}
+
+/**
+ * @brief SPI1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_SPI1_Init(void) {
+
+  /* USER CODE BEGIN SPI1_Init 0 */
+
+  /* USER CODE END SPI1_Init 0 */
+
+  /* USER CODE BEGIN SPI1_Init 1 */
+
+  /* USER CODE END SPI1_Init 1 */
+  /* SPI1 parameter configuration*/
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi1) != HAL_OK) {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI1_Init 2 */
+
+  /* USER CODE END SPI1_Init 2 */
+
+}
+
+/**
+ * @brief USART1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_USART1_UART_Init(void) {
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 9600;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK) {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+ * Enable DMA controller clock
+ */
+static void MX_DMA_Init(void) {
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Channel3_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel3_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel3_IRQn);
+
+}
+
+/**
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_GPIO_Init(void) {
+  GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, RST_Pin | DC_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(CE_GPIO_Port, CE_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pins : RST_Pin CE_Pin DC_Pin */
+  GPIO_InitStruct.Pin = RST_Pin | CE_Pin | DC_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+/////////////////////////////////// не полный буфер ///////////////////////////////////////
+void HAL_UART_IDLE_Callback(UART_HandleTypeDef *huart) {
+  if (huart == &huart1) {
+    __HAL_UART_DISABLE_IT(&huart1, UART_IT_IDLE);
+    HAL_UART_AbortReceive(&huart1);
+    __HAL_UART_CLEAR_IDLEFLAG(&huart1);
+    __HAL_UART_ENABLE_IT(&huart1, UART_IT_IDLE);
+    isErrorData = 1;
+  }
+}
+
+/////////////////////////////////// полный буфер ///////////////////////////////////////
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+  if (huart == &huart1) {
+    __HAL_UART_DISABLE_IT(&huart1, UART_IT_IDLE);
+    HAL_UART_AbortReceive(&huart1);
+    __HAL_UART_CLEAR_IDLEFLAG(&huart1);
+    __HAL_UART_ENABLE_IT(&huart1, UART_IT_IDLE);
+    isReadyData = 1;
+  }
+}
+/* USER CODE END 4 */
+
+/**
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1) {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
