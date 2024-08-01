c27755e72b2ff8f4bfc0510cfd507c877fad1d45
sergeyerofeev <sergeyexport@yandex.ru>
2024-08-01 21:57:14 +0300

    Update

--- stepper_motor_test/Core/Inc/main.h
+++ stepper_motor_test/Core/Inc/main.h
@@ -63,11 +63,67 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define EN_Pin GPIO_PIN_0
-#define EN_GPIO_Port GPIOB
+#define C13_Pin GPIO_PIN_13
+#define C13_GPIO_Port GPIOC
+#define C14_Pin GPIO_PIN_14
+#define C14_GPIO_Port GPIOC
+#define C15_Pin GPIO_PIN_15
+#define C15_GPIO_Port GPIOC
+#define A1_Pin GPIO_PIN_1
+#define A1_GPIO_Port GPIOA
+#define A2_Pin GPIO_PIN_2
+#define A2_GPIO_Port GPIOA
+#define EN_Pin GPIO_PIN_3
+#define EN_GPIO_Port GPIOA
+#define MS_1_Pin GPIO_PIN_4
+#define MS_1_GPIO_Port GPIOA
+#define MS_2_Pin GPIO_PIN_5
+#define MS_2_GPIO_Port GPIOA
+#define MS_3_Pin GPIO_PIN_6
+#define MS_3_GPIO_Port GPIOA
+#define DIR_Pin GPIO_PIN_7
+#define DIR_GPIO_Port GPIOA
+#define B0_Pin GPIO_PIN_0
+#define B0_GPIO_Port GPIOB
+#define B1_Pin GPIO_PIN_1
+#define B1_GPIO_Port GPIOB
+#define B2_Pin GPIO_PIN_2
+#define B2_GPIO_Port GPIOB
+#define B10_Pin GPIO_PIN_10
+#define B10_GPIO_Port GPIOB
+#define B11_Pin GPIO_PIN_11
+#define B11_GPIO_Port GPIOB
+#define LED_Pin GPIO_PIN_12
+#define LED_GPIO_Port GPIOB
+#define B13_Pin GPIO_PIN_13
+#define B13_GPIO_Port GPIOB
+#define B14_Pin GPIO_PIN_14
+#define B14_GPIO_Port GPIOB
+#define B15_Pin GPIO_PIN_15
+#define B15_GPIO_Port GPIOB
+#define A8_Pin GPIO_PIN_8
+#define A8_GPIO_Port GPIOA
+#define A9_Pin GPIO_PIN_9
+#define A9_GPIO_Port GPIOA
+#define A10_Pin GPIO_PIN_10
+#define A10_GPIO_Port GPIOA
+#define A15_Pin GPIO_PIN_15
+#define A15_GPIO_Port GPIOA
+#define B3_Pin GPIO_PIN_3
+#define B3_GPIO_Port GPIOB
 #define USB_DETECT_Pin GPIO_PIN_4
 #define USB_DETECT_GPIO_Port GPIOB
 #define USB_DETECT_EXTI_IRQn EXTI4_IRQn
+#define B5_Pin GPIO_PIN_5
+#define B5_GPIO_Port GPIOB
+#define B6_Pin GPIO_PIN_6
+#define B6_GPIO_Port GPIOB
+#define B7_Pin GPIO_PIN_7
+#define B7_GPIO_Port GPIOB
+#define B8_Pin GPIO_PIN_8
+#define B8_GPIO_Port GPIOB
+#define B9_Pin GPIO_PIN_9
+#define B9_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
