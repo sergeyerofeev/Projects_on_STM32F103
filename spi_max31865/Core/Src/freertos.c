b2f135951697308308a7320981d3b100452472ae
sergeyerofeev <sergeyexport@yandex.ru>
2026-02-11 15:41:34 +0300

    Update

--- spi_max31865/Core/Src/freertos.c
+++ spi_max31865/Core/Src/freertos.c
@@ -63,7 +63,6 @@ extern TIM_HandleTypeDef htim4;
 extern varReceivingUSB_t receivingData;
 extern MAX31865_t hMAX31865;
 varMAX31865_t varData;
-float result = 0.0f;
 /* USER CODE END Variables */
 /* Definitions for defaultTask */
 osThreadId_t defaultTaskHandle;
@@ -153,17 +152,22 @@ void StartDefaultTask(void *argument) {
   MX_USB_DEVICE_Init();
   /* USER CODE BEGIN StartDefaultTask */
   TickType_t xLastWakeTime = xTaskGetTickCount();
-  const float dt = 0.5f;
-  const float setPoint = 230.0f;    // Целевая температура
+  float dt = DT / 1000.0f;
+  const float setPoint = 220.0f;        // Целевая температура
+
+  float deltaPerCycle = 4 * dt;         // Скорость нагрева 2°C/цикл
+  float deltaTemp = 0.0f;               // Разность между уставкой setPoint и текущй температурой
+  float boostKp = setPoint / 5.0f;      // На данном участке Kp увеличивается
+  float limitedSetpoint = 0.0f;         // Ограниченная уставка
+  float deltaSetpoint = 0.0f;
 
   const float minOut = 0.0f;
   //float maxOut = (float) htim4.Init.Period + 1;
   const float maxOut = 4800.0f;
-  //float kp = receivingData.kp;
   float errorCurrent = 0.0f;
   float errorIntegral = 0.0f;
-  float errorDifferential = 0.0f;
-  //float result = 0.0f;
+
+  float result = 0.0f;
 
   // Сбрасываем линию USB_DP при перезагрузке микроконтроллера
   HAL_GPIO_WritePin(DP_RESET_GPIO_Port, DP_RESET_Pin, GPIO_PIN_RESET);
@@ -176,27 +180,42 @@ void StartDefaultTask(void *argument) {
       // Проверяем, активен ли выход канала 3, если нет, то запускаем ШИМ
       if ((TIM4->CCER & TIM_CCER_CC3E) == 0) {
         HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_3);
+        limitedSetpoint = varData.temp;  // Начинаем с текущей температуры
       }
-      // Вычисление ошибки, разница между уставкой и измеренной температурой
-      errorCurrent = setPoint - varData.temp;
-      /*if (errorCurrent > 10) {
-        kp = receivingData.kp;
+
+      // Вычисляем разность между целевой и ограниченной уставками
+      deltaSetpoint = setPoint - limitedSetpoint;
+
+      // Применение ограничения скорости
+      if (deltaSetpoint > deltaPerCycle) {
+        // Нагревание, с каждым циклом увеличиваем limitedSetpoint на 2°C/цикл
+        limitedSetpoint += deltaPerCycle;
+      } else if (deltaSetpoint < -deltaPerCycle) {
+        // Остывание, с каждым циклом уменьшаем limited_setpoint на 2°C/цикл
+        limitedSetpoint -= deltaPerCycle;
       } else {
-        kp = receivingData.kp * 1.5;
+        // Температура достигла уставки
+        limitedSetpoint = setPoint;
       }
-*/
-      /*errorDifferential = (errorCurrent - errorPrevious) * receivingData.kd / dt;
-       errorPrevious = errorCurrent;*/
 
-      result = errorCurrent * receivingData.kp + errorIntegral + errorDifferential;
+      // Вычисление ошибки, разница между уставкой и измеренной температурой
+      errorCurrent = limitedSetpoint - varData.temp;
 
-      // Проверяем насыщение выхода и если необходимо замораживаем интегральную составляющую
+      errorIntegral += errorCurrent * dt * receivingData.ki / 5.0f;
+
+      deltaTemp = setPoint - varData.temp;
+      if (deltaTemp < boostKp && deltaTemp > 0) {
+       // Увеличение коэффициента kp включаем только при приближении к setPoint, снизу
+        result = errorCurrent * receivingData.kp * 2 + errorIntegral;
+      } else {
+        result = errorCurrent * receivingData.kp + errorIntegral;
+      }
+
+      // Проверяем насыщение выхода
       if (result >= maxOut) {
         result = maxOut;
       } else if (result <= minOut) {
         result = minOut;
-      } else {
-        errorIntegral += errorCurrent * dt * receivingData.ki / 5.0f;
       }
 
       htim4.Instance->CCR3 = (uint16_t) result;
@@ -205,7 +224,7 @@ void StartDefaultTask(void *argument) {
       if (TIM4->CCER & TIM_CCER_CC3E) {
         HAL_TIM_PWM_Stop(&htim4, TIM_CHANNEL_3);
         htim4.Instance->CCR3 = 0;
-        errorIntegral = 0.0f;
+        errorIntegral = 400.0f;
       }
     }
     vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(DT));
@@ -223,7 +242,6 @@ void vErrorHandler() {
 // Callback фукнция задачи получения значения температуры с датчика MAX31865
 void vTaskGetTemp(void *argument) {
   TickType_t xLastWakeTime = xTaskGetTickCount();
-  //uint8_t show1s = 0;
   bool flagShow = true;
 
   for (;;) {
@@ -234,9 +252,6 @@ void vTaskGetTemp(void *argument) {
       xTaskNotifyGive(taskShowHandle);
     }
     flagShow ^= true;
-    /*      show1s = 0;
-     } else
-     show1s++;*/
     vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(DT));
   }
 }
@@ -261,7 +276,7 @@ void vTaskShow(void *argument) {
     ssd1306_SetCursor(x, y);
     ssd1306_WriteString(tempStr, Font_7x10, White);
 
-    length = floatToString(resStr, 10, result);
+    length = floatToString(resStr, 10, varData.res);
     // Выводим значение сопротивления датчика PT100 на дисплей
     x = (SSD1306_WIDTH - length * Font_7x10.width) / 2;
     y = yMiddle + 10;
