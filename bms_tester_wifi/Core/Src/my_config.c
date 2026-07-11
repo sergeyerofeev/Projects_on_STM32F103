1acbc92539f67dcbcb7545cfb4022139dc43bda6
sergeyerofeev <sergeyexport@yandex.ru>
2026-07-11 03:53:38 +0300

    Update

--- bms_tester_wifi/Core/Src/my_config.c
+++ bms_tester_wifi/Core/Src/my_config.c
@@ -23,7 +23,6 @@ const size_t bmsDataCount = sizeof(bmsData) / sizeof(bmsData[0]);
 // ---------------------------------------------------------------------------------------
 
 // Массив структур для поиска вендора по коду
-/*
 CodeMapping mappings[] = {
     {0x4E344007, "Urent"},
     {0x4E34300A, "Teltonika"},
@@ -37,7 +36,6 @@ CodeMapping mappings[] = {
     {0x4E344005, "Molniya"},
     {0x4E34400A, "Swing"},
 };
-*/
 
 char str[STR_SIZE] = { 0, };
 
